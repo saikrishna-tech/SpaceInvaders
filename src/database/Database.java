76609496522150e4cc5b7945ea79391de0427dc2
Sai Krishna Pothini <29saikrishna102002@gmail.com>
2026-07-02 23:09:47 -1200

    feat: estlablish JDBC connection to PostgreSQL

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,5 +1,19 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Database {
-    
+
+    private static final String URL =
+            "jdbc:postgresql://localhost:5432/space_invaders";
+
+    private static final String USER = "postgres";
+
+    private static final String PASSWORD = "Postgres123";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
