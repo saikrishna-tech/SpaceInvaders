85aafa76022d58d19b02d3a1ca68fae672f09510
Sai Krishna Pothini <29saikrishna102002@gmail.com>
2026-07-02 17:34:47 -1200

    feat: add player spaceship

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -7,12 +7,28 @@ public class GamePanel extends JPanel {
 
     public static final int WIDTH = 800;
     public static final int HEIGHT = 600;
+    private Player player;
+
+    @Override
+    protected void paintComponent(java.awt.Graphics g) {
+
+        super.paintComponent(g);
+
+        player.draw(g);
+
+    }
 
     public GamePanel() {
 
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
 
         setBackground(Color.BLACK);
+
+        
+        player = new Player(
+                WIDTH / 2 - Player.WIDTH / 2,
+                HEIGHT - 50
+        );
 
     }
 
--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,18 @@
 public class Player {
-    
+
+    private int x;
+    private int y;
+
+    public static final int WIDTH = 60;
+    public static final int HEIGHT = 20;
+
+    public Player(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void draw(java.awt.Graphics g) {
+        g.fillRect(x, y, WIDTH, HEIGHT);
+    }
+
 }
