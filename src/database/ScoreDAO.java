70f884f24394b3aa5a64915f80abb023d8c3a096
Sai Krishna Pothini <29saikrishna102002@gmail.com>
2026-07-03 23:27:29 -1200

    feat: persist player scores using JDBC

--- src/database/ScoreDAO.java
+++ src/database/ScoreDAO.java
@@ -1,5 +1,30 @@
 package database;
 
+import model.PlayerScore;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ScoreDAO {
-    
+
+    public void saveScore(PlayerScore playerScore) throws SQLException {
+
+        String sql =
+                "INSERT INTO player_scores (player_name, score) VALUES (?, ?)";
+
+        try (
+                Connection connection = Database.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql)
+        ) {
+
+            statement.setString(1, playerScore.getPlayerName());
+            statement.setInt(2, playerScore.getScore());
+
+            int rowsAffected = statement.executeUpdate();
+
+            System.out.println(rowsAffected + " row inserted successfully.");
+
+        }
+    }
 }
--- src/model/PlayerScore.java
+++ src/model/PlayerScore.java
@@ -1,5 +1,20 @@
 package model;
 
 public class PlayerScore {
-    
+
+    private String playerName;
+    private int score;
+
+    public PlayerScore(String playerName, int score) {
+        this.playerName = playerName;
+        this.score = score;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public int getScore() {
+        return score;
+    }
 }
